791693d074ae82690a8cdc410d48a079aa7b4c35
kazukimastumoto <uver.loveloveleke@icloud.com>
2021-01-27 23:12:07 +0900

    アソシエーションの追加及び外部キー設定

--- db/migrate/20210122082909_create_notices.rb
+++ db/migrate/20210122082909_create_notices.rb
@@ -1,11 +1,11 @@
 class CreateNotices < ActiveRecord::Migration[6.0]
   def change
     create_table :notices do |t|
-      t.string :notice_title,       null:false
-      t.string :notice_display,     null:false
-      t.string :notice_heading,     null:false
-      t.text   :notice_description, null:false
-
+      t.string  :notice_title,       null:false
+      t.string  :notice_display,     null:false
+      t.string  :notice_heading,     null:false
+      t.text    :notice_description, null:false
+      t.integer :store_id,           null:false
       t.timestamps
     end
   end
