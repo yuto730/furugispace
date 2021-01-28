916986fa49c6cc1084088af531162e60e3423aa3
yuto730 <my30.design@gmail.com>
2021-01-28 16:13:26 +0900

    communityページの作成

---
+++ app/controllers/communities_controller.rb
@@ -0,0 +1,25 @@
+class CommunitiesController < ApplicationController
+  def index
+    @communities = Community.page(params[:page]).per(1)
+  end
+
+  def new
+    @community = Community.new
+  end
+
+  def create
+    @community = Community.create(community_params)
+    if @community.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def community_params
+    params.require(:community).permit(:community_title, :community_profile).merge(user_id: current_user.id, store_id: current_store.id)
+  end
+
+end
