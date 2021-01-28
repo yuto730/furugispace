15f5adf78f504e74d8e029cf111244145a85f726
yuto730 <my30.design@gmail.com>
2021-01-28 18:23:32 +0900

    コミュニティ新規作成ページの実装

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'kaminari'
 gem 'react-rails'
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/models/community.rb
+++ app/models/community.rb
@@ -2,6 +2,7 @@ class Community < ApplicationRecord
 
   belongs_to :store
   belongs_to :user
+  has_one_attached :image
 
   with_options presence: true do
     validates :community_title
