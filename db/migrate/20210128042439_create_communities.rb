5bb4a1a6dffe4854073f014ac750c7e7b36f85b9
yuto730 <my30.design@gmail.com>
2021-02-01 22:31:37 +0900

    コミュニティの新規作成

--- db/migrate/20210128042439_create_communities.rb
+++ db/migrate/20210128042439_create_communities.rb
@@ -3,7 +3,6 @@ class CreateCommunities < ActiveRecord::Migration[6.0]
     create_table :communities do |t|
       t.string     :community_title,   null: false
       t.text       :community_profile, null: false
-      t.references :store,             null: false, foreign_key: true
       t.references :user,              null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -36,11 +36,9 @@ ActiveRecord::Schema.define(version: 2021_01_29_050039) do
   create_table "communities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "community_title", null: false
     t.text "community_profile", null: false
-    t.bigint "store_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["store_id"], name: "index_communities_on_store_id"
     t.index ["user_id"], name: "index_communities_on_user_id"
   end
 
@@ -96,7 +94,6 @@ ActiveRecord::Schema.define(version: 2021_01_29_050039) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "communities", "stores"
   add_foreign_key "communities", "users"
   add_foreign_key "coordinations", "stores"
   add_foreign_key "coordinations", "users"
