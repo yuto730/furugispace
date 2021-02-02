a4cfdd805be085bd3a0fc2837c76b549dbdd8d5b
yuto730 <my30.design@gmail.com>
2021-02-02 16:26:01 +0900

    店舗ユーザー新規会員登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_nickname, :store_nickname, :address, :phone_number])
   end
 end
