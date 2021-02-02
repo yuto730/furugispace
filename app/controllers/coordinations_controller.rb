95e7869b3be92f3a37a5e05bd2d71c6ccc116c12
yuto730 <my30.design@gmail.com>
2021-02-02 15:18:46 +0900

    coordination_新規作成

--- app/controllers/coordinations_controller.rb
+++ app/controllers/coordinations_controller.rb
@@ -4,6 +4,25 @@ class CoordinationsController < ApplicationController
     @coordinations = Coordination.all
   end
 
+  def new
+    @coordination = Coordination.new
+  end
+
+  def create
+    @coordination = Coordination.create(coordination_params)
+    if @coordination.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def show
+  end
+
+  private
+
+  def coordination_params
+    params.require(:coordination).permit(:coordination_title, :image, :coordination_profile, :item_id, :item_text).merge(user_id: current_user.id)
   end
 end
--- db/migrate/20210129050039_create_coordinations.rb
+++ db/migrate/20210129050039_create_coordinations.rb
@@ -1,10 +1,11 @@
 class CreateCoordinations < ActiveRecord::Migration[6.0]
   def change
     create_table :coordinations do |t|
-      t.integer    :item_id
-      t.string     :item_text
-      t.references :store, null: false, foreign_key: true
-      t.references :user,  null: false, foreign_key: true
+      t.string     :coordination_title,  null: false
+      t.text       :coordination_profile
+      t.integer    :item_id,             null: false
+      t.string     :item_text,           null: false
+      t.references :user,                null: false, foreign_key: true
 
       t.timestamps
     end
