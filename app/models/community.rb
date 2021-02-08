3c6d320ccca983eed026ba6bdc0ebb9805d1f17c
yuto730 <my30.design@gmail.com>
2021-02-08 15:42:42 +0900

    チャットルームの作成

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,5 +1,8 @@
 class Community < ApplicationRecord
   belongs_to :user
+  has_many :community_users
+  has_many :users, through: :community_users
+
   has_one_attached :image
 
   with_options presence: true do
