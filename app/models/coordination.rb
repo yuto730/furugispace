5c5a99c7d666b57aa2d6b7b616116de20c69062e
yuto730 <my30.design@gmail.com>
2021-02-24 23:37:46 +0900

    cocoonの導入

--- app/models/coordination.rb
+++ app/models/coordination.rb
@@ -1,6 +1,9 @@
 class Coordination < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :item
+  has_many      :items, dependent: :destroy
+  accepts_nested_attributes_for :items, allow_destroy: true
+
   belongs_to :user
   has_many :comments
 
