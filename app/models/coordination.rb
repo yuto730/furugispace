d730fad0fcf45777327fb1becdbdc6b1b35d6b2e
yuto730 <my30.design@gmail.com>
2021-04-05 15:04:48 +0900

    【通知機能】モデルの作成

--- app/models/coordination.rb
+++ app/models/coordination.rb
@@ -3,9 +3,31 @@ class Coordination < ApplicationRecord
   belongs_to :user
   has_many :coordination_items, dependent: :destroy
   accepts_nested_attributes_for :coordination_items
-  has_many :comments
+  has_many :comments, dependent: :destroy
+  has_many :notifications, dependent: :destroy
 
   has_one_attached :image
 
   validates :coordination_title, presence: true
+
+  def create_notification_comment!(current_user, comment_id)
+    temp_ids = Comment.select(:user_id).where(coordination_id: id).where.not(user_id: current_user.id).distinct
+    temp_ids.each do |temp_id|
+      save_notification_comment!(current_user, comment_id, temp_id['user_id'])
+    end
+    save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank?
+  end
+
+  def save_notification_comment!(current_user, comment_id, visited_id)
+    notification = current_user.active_notifications.new(
+      coordination_id: id,
+      comment_id: comment_id,
+      visited_id: visited_id,
+      action: 'comment'
+    )
+    if notification.visitor_id == notification.visited_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,9 @@ class User < ApplicationRecord
   has_many :coordinations
   has_many :comments
 
+  has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
+  has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
+
   has_one_attached :avatar
 
   validates :user_nickname, presence: true
@@ -29,5 +32,16 @@ class User < ApplicationRecord
 
   def unfollow(other_user)
     following_relationships.find_by(following_id: other_user.id).destroy
+  end
+
+  def create_notification_follow!(current_user)
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and action = ? ",current_user.id, id, 'follow'])
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        visited_id: id,
+        action: 'follow'
+      )
+      notification.save if notification.valid?
+    end
   end
 end
